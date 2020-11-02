9c6e50af022d0d146814f9c771a54b2062a112b5
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-02 16:11:12 +0530

    Create If1.c
    1.Accept two numbers and find greater of them::

---
+++ If1.c
@@ -0,0 +1,17 @@
+//1.Accept two numbers and find greater of them::
+
+#include<stdio.h>
+int main()
+{
+  int num1,num2;
+  printf("Enter two numbers:");
+  scanf("%d%d",&num1,num2);
+  
+  if(num1>num2)
+   printf("%d is greater",num1);
+  
+  if(num1<num2)
+    printf("%d is greater",num2);
+    
+  return 0;
+}  
